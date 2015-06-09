ac466fee8840cd090b95947463f1808acf4e8244
mel <mel.michael@andela.co>
2015-06-09 02:57:13 +0100

    Set background color when active and show sync state

--- js/controller.js
+++ js/controller.js
@@ -2,4 +2,13 @@
 
 app.controller('NoteCtrl', ['$scope', function($scope) {
   $scope.test = 'working!';
+
+  $scope.showNote = function() {
+    $scope.activeNote = true;
+    $scope.syncNote   = true;
+
+    //If syncing is NOT in progress set syncing to false
+    // $scope.syncing = false;
+  };
+
 }]);
