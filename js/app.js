ca2d89dc22c55c9f456faae1350f1be5d714a3be
mel <mel.michael@andela.co>
2015-06-11 12:36:41 +0100

    Impement delete note

--- js/app.js
+++ js/app.js
@@ -17,6 +17,32 @@ var app = angular.module('andeNote', [])
       replace: true
     };
   })
+  .directive('noteList', function(){
+    return{
+      restrict: 'E',
+      templateUrl:'templates/note-list.html',
+      replace: true,
+      controller: ['$scope', function($scope) {
+        $scope.deleteNote = function(note,key) {
+          var response = confirm("Are you sure you want to delete this note");
+          if(response === true){
+            delete $scope.allNotes[key];
+          }
+        };
+
+      }],
+      link: function(scope, elem, attr) {
+        var el = elem.find('.single-list');
+        var sideBar = angular.element('.sidebar');
+        var delBtn = el.find('.deleteBtn');
+
+        elem.find('.list-row__content').on('click', function(){
+          sideBar.find('li').removeClass('active');
+          $(elem).addClass('active');
+        });
+      }
+    }
+  })
   .directive('noteArea', function() {
     return {
       restrict: 'E',
