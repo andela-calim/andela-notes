f200a7fb61c53a5ee9d3f87467c5af6723df14da
mel <mel.michael@andela.co>
2015-06-11 18:57:32 +0100

    Fix add button and background color

--- js/app.js
+++ js/app.js
@@ -23,10 +23,10 @@ var app = angular.module('andeNote', [])
       templateUrl:'templates/note-list.html',
       replace: true,
       controller: ['$scope', function($scope) {
-        $scope.deleteNote = function(note,key) {
+        $scope.deleteNote = function(note, key) {
           var response = confirm("Are you sure you want to delete this note");
           if(response === true){
-            delete $scope.allNotes[key];
+            $scope.allNotes.splice($scope.allNotes.indexOf(note), key);
           }
         };
 
@@ -35,19 +35,10 @@ var app = angular.module('andeNote', [])
         var el = elem.find('.single-list');
         var sideBar = angular.element('.sidebar');
         var delBtn = el.find('.deleteBtn');
-        // var syncWhite = el.find('.icon--white');
-        var syncGreen = el.find('.icon--green');
-        console.log(syncGreen);
 
         elem.find('.list-row__content').on('click', function(){
           sideBar.find('li').removeClass('active');
           $(elem).addClass('active');
-        });
-        elem.find('.list-row__content').on('click', function(){
-          el.find('.icon--green').removeAttr("style");
-          el.find('.icon--green').css("visibility", "visible");
-          // syncWhite.css('visibility': 'hidden');
-          // syncGreen.css('visibility': 'visible');
         });
       }
     }
