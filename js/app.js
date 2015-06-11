ceea2aac0732232e778409913c8bb208e08f8b6c
Adeleke Togun <adeleketogun@gmail.com>
2015-06-11 01:55:24 +0100

    Setup mobile layout

--- js/app.js
+++ js/app.js
@@ -2,7 +2,23 @@
 // $.material.init();
 
 var app = angular.module('andeNote', [])
-
+.directive('mobileHeader', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/mobile-header.html',
+    replace: true,
+    link: function (scope, element) {
+      var sidePane = new $.slidebars();
+      var toggle = false;
+      $('.nav-call').on('click', function() {
+        toggle = !toggle;
+        if (toggle !== false) {
+          sidePane.slidebars.open('left');
+        }
+      });
+    }
+  };
+})
 .directive('header', function() {
     return {
       restrict: 'E',
